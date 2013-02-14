221dab7aac7b94ffff0052cd01ff3c894bec833e
Kush Kella <kush.kella@gmail.com>
2013-02-14 20:34:38 +0500

    Added conditions to resource to play along with activerecord resources.

--- lib/active_admin/mongoid/resource.rb
+++ lib/active_admin/mongoid/resource.rb
@@ -4,7 +4,11 @@ require 'inherited_resources'
 ActiveAdmin::Resource # autoload
 class ActiveAdmin::Resource
   def resource_table_name
-    resource_class.collection_name
+    if (resource_class.included_modules.include? Mongoid::Document)
+      return resource_class.collection_name
+    else
+      return resource_class.quoted_table_name
+    end
   end
 end
 
@@ -15,22 +19,44 @@ class ActiveAdmin::ResourceController
   protected
 
   def skip_sidebar!
-    @skip_sidebar = true
+	if(resource_class.included_modules.include? Mongoid::Document)
+		@skip_sidebar = true
+	end
   end
 
   # Use #desc and #asc for sorting.
   def sort_order(chain)
-    params[:order] ||= active_admin_config.sort_order
-    table_name = active_admin_config.resource_table_name
-    if params[:order] && params[:order] =~ /^([\w\_\.]+)_(desc|asc)$/
-      chain.send($2, $1)
-    else
-      chain # just return the chain
-    end
+	if (resource_class.included_modules.include? Mongoid::Document)
+      params[:order] ||= active_admin_config.sort_order
+      table_name = active_admin_config.resource_table_name
+      if params[:order] && params[:order] =~ /^([\w\_\.]+)_(desc|asc)$/
+        chain.send($2, $1)
+      else
+        chain # just return the chain
+      end
+	else
+	  params[:order] ||= active_admin_config.sort_order
+	  if params[:order] && params[:order] =~ /^([\w\_\.]+)_(desc|asc)$/
+		column = $1
+		order  = $2
+		table  = active_admin_config.resource_table_name
+		table_column = (column =~ /\./) ? column :
+		  "#{table}.#{active_admin_config.resource_quoted_column_name(column)}"
+
+		chain.reorder("#{table_column} #{order}")
+	  else
+		chain # just return the chain
+	  end
+	end
   end
 
   # Disable filters
   def search(chain)
-    chain
+	if (resource_class.included_modules.include? Mongoid::Document)
+		chain
+	else
+		@search = chain.metasearch(clean_search_params(params[:q]))
+		@search.relation
+	end
   end
 end
